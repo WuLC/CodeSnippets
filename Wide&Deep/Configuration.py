6d2aa5a824d60a1ea8a1176d71b85392d69374ab
WuLC <liangchaowu5@gmail.com>
2018-06-05 11:00:44 +0800

    Add more choices

--- Wide&Deep/Configuration.py
+++ Wide&Deep/Configuration.py
@@ -4,7 +4,7 @@ class Configuration:
     """A class that stores variables specified by user"""
     def __init__(self):
         # which dataset to use
-        self.dataset = 'criteo'
+        self.dataset = 'criteo' # avazu, ipinyou
 
         # environment of visdom
         self.visualize = True
@@ -14,10 +14,10 @@ class Configuration:
         self.num_train = 1200000# 150000 # 3000000
         self.num_val =   160000 # 20000   #400000
         if platform.system() == 'Linux':
-            self.data_dir = '/mnt/e/dataset/criteo'
+            self.data_dir = '/mnt/e/dataset/{0}'.format(self.dataset)
             self.model_dir = '/mnt/e/{0}_saved_models'.format(self.num_train)
         elif platform.system() == 'Windows':
-            self.data_dir = 'E:/dataset/criteo'
+            self.data_dir = 'E:/dataset/{0}'.format(self.dataset)
             self.model_dir = 'E:/{0}_saved_models'.format(self.num_train)
         self.num_continous_feature = 13
         self.num_categorical_feature = 26
@@ -35,9 +35,10 @@ class Configuration:
         self.num_readers = 5
 
         # model parameters
-        self.model_type = 'wdl' # 'wide', 'deep' or 'wdl'
+        self.model_type = 'wide' # 'wide', 'deep' or 'wdl'
         self.use_fm_vector = False  # initialize embedding layer with latent vector obtained by FM
         self.hidden_units = [100, 75, 50, 25] # hidden units for deep part
         self.max_hash_size = 1000000
-        self.embedding_size = 24
+        self.embedding_size = 12
         self.loss_fn = 'log_loss' # weighted_log_loss, log_loss, focal_loss
+        self.drop_out = 0.0 # drop_out_rate
