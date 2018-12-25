1319ff8f94c7afe039db88237692f4357857e138
WuLC <liangchaowu5@gmail.com>
2018-12-25 15:12:25 +0800

    Serial crawler

---
+++ CrawlerInGo/Crawler.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Fetcher interface {
+	// Fetch returns the body of URL and
+	// a slice of URLs found on that page.
+	Fetch(url string) (body string, urls []string, err error)
+}
+
+type fakeFetcher map[string]*fakeResult
+
+type fakeResult struct {
+	body string
+	urls []string
+}
+
+func (f fakeFetcher) Fetch(url string) (string, []string, error) {
+	if res, ok := f[url]; ok {
+		return res.body, res.urls, nil
+	}
+	return "", nil, fmt.Errorf("not found: %s", url)
+}
+
+//////////////////////////////////////////////////
+// three implementations of the Crawl method
+//////////////////////////////////////////////////
+
+// implementation 1, single goroutine
+
+func CrawlSerial(url string, fetcher fakeFetcher, fetched map[string]bool) {
+	if _, ok := fetched[url]; ok {
+		return
+	}
+	fetched[url] = true
+	body, urls, err := fetcher.Fetch(url)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("found: %s %q\n", url, body)
+	for _, u := range urls {
+		CrawlSerial(u, fetcher, fetched)
+	}
+	return
+}
+
+func main() {
+	var f = fakeFetcher{
+		"https://golang.org/": &fakeResult{
+			"The Go Programming Language",
+			[]string{
+				"https://golang.org/pkg/",
+				"https://golang.org/cmd/",
+			},
+		},
+		"https://golang.org/pkg/": &fakeResult{
+			"Packages",
+			[]string{
+				"https://golang.org/",
+				"https://golang.org/cmd/",
+				"https://golang.org/pkg/fmt/",
+				"https://golang.org/pkg/os/",
+			},
+		},
+		"https://golang.org/pkg/fmt/": &fakeResult{
+			"Package fmt",
+			[]string{
+				"https://golang.org/",
+				"https://golang.org/pkg/",
+			},
+		},
+		"https://golang.org/pkg/os/": &fakeResult{
+			"Package os",
+			[]string{
+				"https://golang.org/",
+				"https://golang.org/pkg/",
+			},
+		},
+	}
+	CrawlSerial("https://golang.org/", f, map[string]bool{})
+}
---
+++ CrawlerInGo/README.md
@@ -0,0 +1,3 @@
+# parallel crawler
+
+A Tour of Go 最后的 [exercise](https://tour.golang.org/concurrency/10): 将一个 serial 的 crawler 改成 parallel 的，可通过两种方式实现: mutex 和 channel
